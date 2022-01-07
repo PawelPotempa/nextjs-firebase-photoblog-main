ffe965c9d975b2ebce5dea0898671e4c9ae1f889
PawelPotempa <80518612+PawelPotempa@users.noreply.github.com>
2022-01-07 20:27:01 +0100

    another attempt at iOS fix

--- styles/singlepostElements.js
+++ styles/singlepostElements.js
@@ -71,7 +71,7 @@ export const SPGalleryItem = styled.div`
   display: flex;
   overflow: hidden;
   width: 100%;
-  height: 100%;
+  /* height: 100%; */
   object-fit: cover;
   transition: all 0.5s ease;
 
